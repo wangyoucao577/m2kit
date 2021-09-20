14b556360b2e52ff720e42a91d53b2cbf71b4426
Jay Zhang <wangyoucao577@gmail.com>
2021-09-20 07:33:58 +0000

    feat: package correct libs

--- third-party/boringssl/conanfile.py
+++ third-party/boringssl/conanfile.py
@@ -8,7 +8,7 @@ class BoringsslConan(ConanFile):
     author = "Jay Zhang <wangyoucao577@gmail.com>"
     url = "https://github.com/google/boringssl"
     description = "BoringSSL is a fork of OpenSSL that is designed to meet Google's needs."
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    topics = ("boringssl", "openssl", "ssl", "tls", "encryption", "security")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
@@ -21,7 +21,7 @@ class BoringsslConan(ConanFile):
 
     def build(self):
         cmake = CMake(self, generator="Ninja")
-        cmake.verbose = True
+        #cmake.verbose = True
         cmake.configure(source_folder="src")
         cmake.build()
 
@@ -31,12 +31,13 @@ class BoringsslConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="src/boringssl/include", keep_path=True)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        if self.settings.os == "Windows":
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
+            self.copy("*.dylib*", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["boringssl"]
