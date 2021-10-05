a8205963e8e59df5b8667a22ee9958d58ba2e259
Jay Zhang <wangyoucao577@gmail.com>
2021-10-05 03:34:02 +0000

    feat: sync conan profiles so that we can use short name for profile

--- build.py
+++ build.py
@@ -8,8 +8,29 @@ def system(command):
     if retcode != 0:
         raise Exception("Error while executing:\n\t %s" % command)
 
+def sync_conan_profiles():
+    conan_home_path = os.path.join(tools.get_env("HOME"), ".conan")
+    if tools.get_env("CONAN_USER_HOME"):
+        conan_home_path = os.path.join(tools.get_env("CONAN_USER_HOME"), ".conan")
+    conan_profiles_path = os.path.join(conan_home_path, "profiles")
+
+    for root, dirs, files in os.walk(os.path.abspath("buildtools/conan-profiles")):
+        for f in files:
+            src = os.path.join(root, f)
+            dst = os.path.join(conan_profiles_path, f)
+            if os.path.islink(dst) and os.path.realpath(dst) == src:
+                # symlink already correct, nothing need to change
+                continue
+            if os.path.isfile(dst):
+                os.remove(dst)    
+            os.symlink(src, dst)
+            print("created symlink: {} -> {}".format(dst, src))
+
 if __name__ == "__main__":
     params = " ".join(sys.argv[1:])
+
+    # sync project conan profiles on host 
+    sync_conan_profiles()
 
     submodules = ["third-party/boringssl", "third-party/ffmpeg"]
 
